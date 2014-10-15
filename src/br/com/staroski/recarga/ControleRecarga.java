aff0ebc768187c7365569ed45b62d26600a77b5b
Ricardo Artur Staroski <ricardo.staroski@gmail.com>
2014-10-15 18:34:23 -0300

    updated recarga

---
+++ src/br/com/staroski/recarga/ControleRecarga.java
@@ -0,0 +1,17 @@
+package br.com.staroski.recarga;
+
+import javax.swing.*;
+
+public final class ControleRecarga {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("HelloWorld");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(640, 480);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+
+	private ControleRecarga() {
+	}
+}
